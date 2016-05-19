c97e585c3c87180c11d4f1f5e8242caf1734bd7d
yjajkiew <yann.jajkiewicz@gmail.com>
2016-05-19 17:39:01 +0200

    add core module status

---
+++ core/status.js
@@ -0,0 +1,25 @@
+var os = require('os');
+var util = require('util');
+
+var status = {};
+status.name = 'status';
+status.commands = {
+	'en': ['STATUS'],
+	'fr': ['STATUS']
+};
+status.handle = handle;
+
+function handlne(stt, tts, user) {
+
+	var text = {
+		'en': 'I am currently running on %s %s. Free memory is %s of %s. The uptime is %s.',
+		'fr': 'Je suis actuellement sous %s %s. la mémoire libre est de %s sur %s. Le temps de fonctionnement est de %s.'
+	};
+	var response = util.format(text[user.language], os.platform(), os.release(), os.freemem(), os.totalmem(), os.uptime());
+
+	tts.speak(response).on('done', function() {
+		stt.release();
+	});
+};
+
+module.exports = status;
